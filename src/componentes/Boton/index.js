e878490a573f1f6791125bf33b50b11cc60c4bd3
vivir13 <157038159+vivir13@users.noreply.github.com.>
2024-06-05 06:59:28 -0600

    se coloca proyecto

---
+++ src/componentes/Boton/index.js
@@ -0,0 +1,8 @@
+import "./Boton.css"
+
+const Boton = (props) => {
+    return <button className="boton">{props.children}</button>
+}
+
+export default Boton
+
---
+++ src/componentes/Footer/index.jsx
@@ -0,0 +1,22 @@
+
+import "./Footer.css"
+
+const Footer = () => {
+    return <footer className='footer' style={{ backgroundImage: "url(/img/footer.png)" }}>
+        <div className='redes'>
+            <a href='https://www.aluracursos.com/'>
+                <img src="/img/facebook.png" alt='Facebook' />
+            </a>
+            <a href='https://www.aluracursos.com/'>
+                <img src="/img/twitter.png" alt='twitter' />
+            </a>
+            <a href='https://www.aluracursos.com/'>
+                <img src="/img/instagram.png" alt='instagram' />
+            </a>
+        </div>
+        <img src='/img/Logo (1).png' alt='org' />
+        <strong>Desarrollado por Viviana Rios </strong>
+    </footer>
+}
+
+export default Footer
